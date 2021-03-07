db612148007ef8b55e08b2aebf8a03a0b831c354
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2021-03-06 23:53:28 -0500

    Updates to Login to implement with firebase

--- p3_MAIN/lib/loginScreen.dart
+++ p3_MAIN/lib/loginScreen.dart
@@ -1,117 +1,135 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+// TODO: change into stateful widget
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String _pantherid, _password;
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Padding(
           padding: const EdgeInsets.only(left: 25, right: 25),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/loginLogoV1.png',
-                  width: 300,
-                  scale: 1.5,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  "Sign In:",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.normal),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                UserPassLogin(),
-              ],
+          child: Form(
+            key: _formkey,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/loginLogoV1.png',
+                    width: 300,
+                    scale: 1.5,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "Sign In:",
+                    style:
+                        TextStyle(fontSize: 30, fontWeight: FontWeight.normal),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    validator: (input) {
+                      if (input.isEmpty) {
+                        return "Enter Valid ID";
+                      }
+                    },
+                    onSaved: (input) => _pantherid = input,
+                    // USERNAME
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Colors.grey,
+                        ),
+                        hintText: "Panther ID",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: BorderSide(),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: BorderSide(
+                            color: Colors.amber,
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    // Spacing between widgets
+                    height: 10,
+                  ),
+                  TextFormField(
+                      validator: (input) {
+                        if (input.isEmpty) {
+                          return "Please re-enter your password.";
+                        }
+                      },
+                      onSaved: (input) => _password = input,
+                      // PASSWORD
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.grey,
+                          ),
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            borderSide: BorderSide(
+                              color: Colors.amber,
+                            ),
+                          ))),
+                  SizedBox(
+                    // Spacing between widgets
+                    height: 20,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 150,
+                    child: RaisedButton(
+                      color: Colors.amber,
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        return null;
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
-}
 
-class UserPassLogin extends StatelessWidget {
-  const UserPassLogin({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          TextField(
-            // USERNAME
-            decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: Colors.grey,
-                ),
-                hintText: "Panther ID",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: BorderSide(
-                    color: Colors.amber,
-                  ),
-                )),
-          ),
-          SizedBox(
-            // Spacing between widgets
-            height: 10,
-          ),
-          TextField(
-              // PASSWORD
-              obscureText: true,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.grey,
-                  ),
-                  hintText: "Password",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                    borderSide: BorderSide(
-                      color: Colors.amber,
-                    ),
-                  ))),
-          SizedBox(
-            // Spacing between widgets
-            height: 20,
-          ),
-          Container(
-            height: 50,
-            width: 150,
-            child: RaisedButton(
-              color: Colors.amber,
-              child: Text(
-                "Login",
-                style: TextStyle(fontSize: 20),
-              ),
-              onPressed: () {
-                return null;
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
-            ),
-          )
-        ],
-      ),
-    );
+/*
+  void signIn() {
+    final _form = _formkey.currentState;
+    if (_formkey.validate()) {
+      // login to firebase
+    }
   }
+
+*/
 }
