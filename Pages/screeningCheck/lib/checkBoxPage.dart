2eac8e7f040d6340091debd2e5e5423754524010
fnotorious <fnato001@fiu.edu>
2021-03-11 18:31:28 -0500

    Checkbox update

--- Pages/screeningCheck/lib/checkBoxPage.dart
+++ Pages/screeningCheck/lib/checkBoxPage.dart
@@ -7,6 +7,24 @@ class CheckListPageOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const FIUNavyBlue = const Color.fromRGBO(8, 30, 63, 1.0);   // FIU's navy blue color.
+
+    // All checkbox options
+
+    CheckBox fever = CheckBox(title: "Fever (temperature of 100.4°F or higher) or chills");
+    CheckBox cough = CheckBox(title: "Cough");
+    CheckBox breath = CheckBox(title: "Shortness of breath / Difficulty breathing");
+    CheckBox fatigue = CheckBox(title: "Fatigue");
+    CheckBox muscle = CheckBox(title: "Muscle or body aches");
+    CheckBox headaches = CheckBox(title: "Headaches");
+    CheckBox taste = CheckBox(title: "New loss of taste or smell");
+    CheckBox throat = CheckBox(title: "Sore throat");
+    CheckBox nose = CheckBox(title: "Congestion or runny nose");
+    CheckBox nausea = CheckBox(title: "Nausea or vomiting");
+    CheckBox diarrhea = CheckBox(title: "Diarrhea");
+    CheckBox noSymp = CheckBox(title: "I am experiencing no symptoms");
+
+    List options = [fever, cough, breath, fatigue, muscle, headaches, taste, throat, nose, nausea, diarrhea, noSymp];
+
     return Scaffold(
         appBar: AppBar(
           shadowColor: Colors.transparent,
@@ -17,7 +35,7 @@ class CheckListPageOne extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.only(right: 20, left: 20),
-          child: Column(
+          child: ListView(
             children: <Widget>[
               // "Safety Screening Checklist" text.
               Container(
@@ -76,8 +94,82 @@ class CheckListPageOne extends StatelessWidget {
                   ),
                 ),
               ),
+              Container(
+                padding: EdgeInsets.only(top: 20), 
+                alignment: Alignment.topCenter,
+                width: 315,
+                child: Column(
+                  children: <Widget>[
+                    fever,
+                    cough,
+                    breath,
+                    fatigue,
+                    muscle,
+                    headaches,
+                    taste,
+                    throat,
+                    nose,
+                    nausea,
+                    diarrhea,
+                    noSymp,
+                  ],
+                )
+              )
             ],
           ),
         ));
   }
 }
+
+// Checkbox tile stateful widget
+class CheckBox extends StatefulWidget { 
+  final String title;   // Symptom being experienced
+  bool _value = false;
+  bool greyOut;
+
+  CheckBox({Key key, this.title}) : super(key: key);  // Constructor
+
+  // Return whether or not check box is checked
+  bool get getValue{
+    return _value;
+  }
+
+  set setValue(bool aValue){
+    _value = aValue;
+  }
+
+  set setGreyOut(bool aValue){
+    greyOut = aValue;
+  }
+  
+  @override
+  _CheckBoxState createState() => _CheckBoxState();
+}
+
+class _CheckBoxState extends State<CheckBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CheckboxListTile(
+        value: widget._value,
+        contentPadding: const EdgeInsets.only(right: 10, left: 20),
+        controlAffinity: ListTileControlAffinity.platform,
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            fontFamily: "Be Vietnam",
+            fontSize: 15.0,
+            fontWeight: FontWeight.normal,
+          )
+        ),
+        activeColor: Color.fromRGBO(8, 30, 63, 1.0),
+        onChanged: (value) {
+          setState(() {
+            widget._value = value;
+          });
+        }
+      )
+    );
+  }
+}
+
