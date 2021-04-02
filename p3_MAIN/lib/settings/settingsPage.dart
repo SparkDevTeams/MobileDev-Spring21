07c0626d8ba47edf5fb9fcc4eafde5349aa39e20
fnotorious <fnato001@fiu.edu>
2021-04-02 03:30:24 -0400

    Fixed buttons on settings

--- p3_MAIN/lib/settings/settingsPage.dart
+++ p3_MAIN/lib/settings/settingsPage.dart
@@ -11,6 +11,7 @@ class SettingsPage extends StatefulWidget {
   final String firstname, lastname;
   const SettingsPage({Key key, this.firstname, this.lastname})
       : super(key: key);
+  
 
   @override
   _SettingsPageState createState() => _SettingsPageState();
@@ -18,6 +19,71 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   bool _faceIdValue = false;
+  final languageButtons = [
+    // yes and no options for first question
+    ButtonSetting(
+        id: 1,
+        value: true,
+        height: 50.0,
+        width: 100.0,
+        colorBackground: Colors.white,
+        colorText: const Color.fromRGBO(8, 30, 63, 1.0),
+        text: 'English'),
+    ButtonSetting(
+        id: 2,
+        value: false,
+        height: 50.0,
+        width: 100.0,
+        colorBackground: Colors.white,
+        colorText: const Color.fromRGBO(8, 30, 63, 1.0),
+        text: 'Spanish'),
+    ButtonSetting(
+        id: 3,
+        value: false,
+        height: 50.0,
+        width: 100.0,
+        colorBackground: Colors.white,
+        colorText: const Color.fromRGBO(8, 30, 63, 1.0),
+        text: 'Creole'),
+  ];
+
+  final onOffOne = [
+    ButtonSetting(
+        id: 1,
+        value: false,
+        height: 50.0,
+        width: 100.0,
+        colorBackground: Colors.white,
+        colorText: const Color.fromRGBO(8, 30, 63, 1.0),
+        text: 'On'),
+    ButtonSetting(
+        id: 1,
+        value: false,
+        height: 50.0,
+        width: 100.0,
+        colorBackground: Colors.white,
+        colorText: const Color.fromRGBO(8, 30, 63, 1.0),
+        text: 'Off'),
+  ];
+
+  final onOffTwo = [
+    ButtonSetting(
+        id: 2,
+        value: false,
+        height: 50.0,
+        width: 100.0,
+        colorBackground: Colors.white,
+        colorText: const Color.fromRGBO(8, 30, 63, 1.0),
+        text: 'On'),
+    ButtonSetting(
+        id: 2,
+        value: false,
+        height: 50.0,
+        width: 100.0,
+        colorBackground: Colors.white,
+        colorText: const Color.fromRGBO(8, 30, 63, 1.0),
+        text: 'Off'),
+  ];
 
   void nullReturn() {
     return null;
@@ -64,41 +130,21 @@ class _SettingsPageState extends State<SettingsPage> {
             SizedBox(
               height: 10,
             ),
-            Text(
-              "Select Language:",
-              style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: Text(
+                "Select Language:",
+                style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+              ),
             ),
             Row(
               children: <Widget>[
-                RoundedButtons(
-                    "English",
-                    17.0,
-                    50.0,
-                    100.0,
-                    Color.fromRGBO(224, 224, 224, 1.0),
-                    Colors.black,
-                    30.0,
-                    nullReturn),
-                SizedBox(width: 30),
-                RoundedButtons(
-                    "Spanish",
-                    17.0,
-                    50.0,
-                    100.0,
-                    Color.fromRGBO(224, 224, 224, 1.0),
-                    Colors.black,
-                    30.0,
-                    nullReturn),
-                SizedBox(width: 30),
-                RoundedButtons(
-                    "Creole",
-                    17.0,
-                    50.0,
-                    100.0,
-                    Color.fromRGBO(224, 224, 224, 1.0),
-                    Colors.black,
-                    30.0,
-                    nullReturn)
+                buildLanguageButton(languageButtons[0]),
+                SizedBox(width: 21),
+                buildLanguageButton(languageButtons[1]),
+                SizedBox(width: 21),
+                buildLanguageButton(languageButtons[2]),
               ],
             ),
             SizedBox(height: 20),
@@ -140,26 +186,10 @@ class _SettingsPageState extends State<SettingsPage> {
             ),
             SizedBox(height: 10),
             Row(
-              children: [
-                RoundedButtons(
-                    "On",
-                    18.0,
-                    50.0,
-                    100.0,
-                    Color.fromRGBO(224, 224, 224, 1.0),
-                    Colors.black,
-                    30.0,
-                    nullReturn),
+              children: <Widget> [
+                buildOnOffButton(onOffOne[0]),
                 SizedBox(width: 40),
-                RoundedButtons(
-                    "Off",
-                    18.0,
-                    50.0,
-                    100.0,
-                    Color.fromRGBO(224, 224, 224, 1.0),
-                    Colors.black,
-                    30.0,
-                    nullReturn)
+                buildOnOffButton(onOffOne[1])
               ],
             ),
             SizedBox(height: 10),
@@ -170,26 +200,10 @@ class _SettingsPageState extends State<SettingsPage> {
             ),
             SizedBox(height: 10),
             Row(
-              children: <Widget>[
-                RoundedButtons(
-                    "On",
-                    18.0,
-                    50.0,
-                    100.0,
-                    Color.fromRGBO(224, 224, 224, 1.0),
-                    Colors.black,
-                    30.0,
-                    nullReturn),
+              children: <Widget> [
+                buildOnOffButton(onOffTwo[0]),
                 SizedBox(width: 40),
-                RoundedButtons(
-                    "Off",
-                    18.0,
-                    50.0,
-                    100.0,
-                    Color.fromRGBO(224, 224, 224, 1.0),
-                    Colors.black,
-                    30.0,
-                    nullReturn)
+                buildOnOffButton(onOffTwo[1])
               ],
             ),
             SizedBox(height: 40),
@@ -216,4 +230,195 @@ class _SettingsPageState extends State<SettingsPage> {
       ),
     );
   }
+
+  Widget buildOnOffButton(ButtonSetting setting) => buildToggleButton(
+      // Widget for on / off buttons
+
+      setting: setting,
+      onClicked: () {
+        setState(() {
+          if (setting.id == 1)
+          {
+            if (setting.text == 'On')
+            {
+              setting.value = true;
+              setting.colorText = Colors.white;
+              setting.colorBackground = Color.fromRGBO(8, 30, 63, 1.0);
+
+              onOffOne[1].value = false;
+              onOffOne[1].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+              onOffOne[1].colorBackground = Colors.white;
+            }
+
+            else 
+            {
+              setting.value = true;
+              setting.colorText = Colors.white;
+              setting.colorBackground = Color.fromRGBO(8, 30, 63, 1.0);
+
+              onOffOne[0].value = false;
+              onOffOne[0].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+              onOffOne[0].colorBackground = Colors.white;
+            }
+          }
+
+          else
+          {
+            if (setting.text == 'On')
+            {
+              setting.value = true;
+              setting.colorText = Colors.white;
+              setting.colorBackground = Color.fromRGBO(8, 30, 63, 1.0);
+
+              onOffTwo[1].value = false;
+              onOffTwo[1].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+              onOffTwo[1].colorBackground = Colors.white;
+            }
+
+            else 
+            {
+              setting.value = true;
+              setting.colorText = Colors.white;
+              setting.colorBackground = Color.fromRGBO(8, 30, 63, 1.0);
+
+              onOffTwo[0].value = false;
+              onOffTwo[0].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+              onOffTwo[0].colorBackground = Colors.white;
+            }
+          }
+        });
+      });
+
+  Widget buildLanguageButton(ButtonSetting setting) => buildToggleButton(
+      // Widget for language buttons
+
+      setting: setting,
+      onClicked: () {
+        setState(() {
+          if (setting.id == 1)
+          {
+            setting.colorText = Colors.white;
+            setting.colorBackground = Color.fromRGBO(8, 30, 63, 1.0);
+            setting.value = true;
+
+            languageButtons[1].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+            languageButtons[1].colorBackground = Colors.white;
+            languageButtons[1].value = false;
+
+            languageButtons[2].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+            languageButtons[2].colorBackground = Colors.white;
+            languageButtons[2].value = false;
+          }
+
+          else if (setting.id == 2)
+          {
+            setting.colorText = Colors.white;
+            setting.colorBackground = Color.fromRGBO(8, 30, 63, 1.0);
+            setting.value = true;
+
+            languageButtons[0].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+            languageButtons[0].colorBackground = Colors.white;
+            languageButtons[0].value = false;
+
+            languageButtons[2].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+            languageButtons[2].colorBackground = Colors.white;
+            languageButtons[2].value = false;
+          }
+
+          else if (setting.id == 3)
+          {
+            setting.colorText = Colors.white;
+            setting.colorBackground = Color.fromRGBO(8, 30, 63, 1.0);
+            setting.value = true;
+
+            languageButtons[0].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+            languageButtons[0].colorBackground = Colors.white;
+            languageButtons[0].value = false;
+
+            languageButtons[1].colorText = Color.fromRGBO(8, 30, 63, 1.0);
+            languageButtons[1].colorBackground = Colors.white;
+            languageButtons[1].value = false;
+          }
+        });
+      });
+
+  Widget buildToggleButton(
+          {
+          // The actual yes / no button widget, takes properties from ButtonSetting class
+
+          @required
+              ButtonSetting
+                  setting, // object of button setting passed here, contains properties for widget
+
+          @required
+              VoidCallback
+                  onClicked // here, a function is passed that decides how the state of the
+          // yes / no button will be set depending on whether the button
+          // being made is selected or deselected
+          }) =>
+      Container(
+        height: setting.height,
+        width: setting.width,
+        child: DecoratedBox(
+          // class for filling in the button when clicked with color
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(25.0),
+              ),
+              color: setting.colorBackground // set background color for button
+              ),
+          child: Theme(
+            data: Theme.of(context).copyWith(
+                buttonTheme: ButtonTheme.of(context).copyWith(
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap)),
+            child: OutlineButton(
+              textColor:
+                  setting.colorText, // set the color of the text in the button
+              highlightElevation: 0,
+              child: Container(
+                child: Text(
+                  "${setting.text}",
+                  style: new TextStyle(
+                      fontSize: setting.fontSize, // set size of the text
+                      fontFamily: "Be Vietnam"),
+                ),
+              ),
+              highlightedBorderColor: setting.colorBorder,
+              borderSide: BorderSide(color: setting.colorBorder, width: 2),
+              shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(
+                    setting.circularRadius), // set roundness of button
+              ),
+              onPressed: () => onClicked(),
+            ),
+          ),
+        ),
+      );
+}
+
+class ButtonSetting {
+// A class for creating a yes / no button's properties
+
+  String text; // yes or no
+  bool value; // initial value of button (set to false)
+  double height; // height of button
+  double width; // width of button
+  Color colorBackground; // background color of button
+  Color colorText; // color of text
+  Color colorBorder; // color of button's border
+  double circularRadius; // roundness of butoon
+  double fontSize; // size of font
+  int id; // id
+
+  ButtonSetting(
+      {@required this.text,
+      @required this.height,
+      @required this.width,
+      @required this.colorBackground,
+      @required this.colorText,
+      @required this.value,
+      @required this.id,
+      this.colorBorder = const Color.fromRGBO(8, 30, 63, 1.0),
+      this.circularRadius = 30.0,
+      this.fontSize = 17.0,});
 }
